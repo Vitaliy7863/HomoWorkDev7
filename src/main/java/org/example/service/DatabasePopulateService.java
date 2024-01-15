93b67b8b7ede476493330f692676dc081e726762
Віталій <vitaliy.shandar@gmail.com>
2024-01-15 22:14:07 +0200

    Commit

--- src/main/java/org/example/service/DatabasePopulateService.java
+++ src/main/java/org/example/service/DatabasePopulateService.java
@@ -1,16 +1,23 @@
 package org.example.service;
 
 import org.example.db.Database;
+import org.example.db.ReadSqlFile;
+
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class DatabasePopulateService {
 
-    private static final String POPULATE_DB_SQL = "INIT=RUNSCRIPT FROM 'sql/populate_db.sql'";
+    private static final ReadSqlFile readSqlFile = new ReadSqlFile();
+    private static final String POPULATE_DB_SQL = "sql/populate_db.sql";
 
     public static void main(String[] args) {
-        try(Connection connection = Database.getConnection(POPULATE_DB_SQL)) {
-            connection.createStatement();
+        String sql = readSqlFile.read(POPULATE_DB_SQL);
+        try(Connection connection = Database.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.execute();
+            preparedStatement.close();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
